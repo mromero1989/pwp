1bfb217d02d86e15fd691377c1750cdddda31745
Michael Romero <m.romero1989@gmail.com>
2017-10-15 13:12:57 -0600

    Added Persona

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Milestone 1</title>
+	</head>
+	<body>
+		<h1 style="font-family:Helvetica;">Purpose</h1>
+		<p>I want to learn the foundations of front end web development and design.</p>
+		<h1 style="font-family:Helvetica;">Audience</h1>
+		<p>This website will be for professional peers, developers and potential employers.</p>
+		<h1 style="font-family:Helvetica;">Goal</h1>
+		<p>My goal is to showcase my work to obtain an internship or full-time employment.</p>
+		<h1>Persona</h1>
+		<ul>
+			<li>Name:Adam</li>
+			<li>Age: 42</li>
+			<li>Job: Programmer</li>
+			<li>Race: Probably white</li>
+			<li>Scenario: He is on the site to learn more about the person he might hire.</li>
+			<li>Need to know: He needs to see an informative history, portfolio, and contact info about the future employee.</li>
+			<li>Feature: Simple site, easily navigatated, multi platform.</li>
+		</ul>
+	</body>
+</html>
