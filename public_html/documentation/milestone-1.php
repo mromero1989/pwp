b6fa107c2b890024e340939916ccdf9773512f6a
Michael Romero <m.romero1989@gmail.com>
2017-10-18 20:57:37 -0600

    Added header image, added more information to the paragraphs and h1's. Added CSS elements.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,25 +2,27 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
+		<link href="css/style.css" type="text/css" rel="stylesheet"/>
 		<title>Milestone 1</title>
 	</head>
+	<!--I wanted to put an image at the top of the web page-->
+	<header>
+		<div id="wrapperHeader">
+			<div id="header">
+				<img src="images/header.png" alt="logo" />
+			</div>
+		</div>
+	</header>
 	<body>
-		<h1 style="font-family:Helvetica;">Purpose</h1>
-		<p>I want to learn the foundations of front end web development and design.</p>
-		<h1 style="font-family:Helvetica;">Audience</h1>
+		<!--I will work on these in more detail once I know my specialization and what career path I want to take-->
+		<h1>Purpose</h1>
+		<p>I want to learn the foundations of both front end and back end development.</p>
+		<h1>Audience</h1>
 		<p>This website will be for professional peers, developers and potential employers.</p>
-		<h1 style="font-family:Helvetica;">Goal</h1>
-		<p>My goal is to showcase my work to obtain an internship or full-time employment.</p>
-		<h1 style="font-family:Helvetica;">Persona</h1>
-		<ul>
-			<li>Name:Adam</li>
-			<li>Age: 42</li>
-			<li>Job: Programmer</li>
-			<li>Race: White</li>
-			<li>Technology: iMac, iPhone</li>
-			<li>Scenario: Adam is looking for someone to hire for his tech start-up. He is knowledgeable about computing and programming. He is looking for actual clean code writing rather than work experience.</li>
-			<li>Need to know: He needs to see an informative history, portfolio, and contact info about the future employee.</li>
-			<li>Feature: Simple site, easily navigated, multi platform.</li>
-		</ul>
+		<h1>Goal</h1>
+		<p>My goal is to obtain a quality internship or an entry level software development job.</p>
+		<!--Will update persona in more detail, I really suck at things like that-->
+		<h1>Persona</h1>
+		<p>Adam is a 38 year old hiring manager at a small independent software company. He is looking for someone to hire as a junior developer. He wants to see an informative history, portfolio and contact information about this candidate. He needs an easily navigated site which works on multiple platforms. The site must show the candidates skill level or he will be immediately turned off.</p>
 	</body>
 </html>
