cd0e1712f1bcb55f55a1fc16a10254f2791bc6d2
Michael Romero <m.romero1989@gmail.com>
2017-10-25 10:58:36 -0600

    Fixed header

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,23 +5,23 @@
 		<link href="css/style.css" type="text/css" rel="stylesheet"/>
 		<title>Milestone 1</title>
 	</head>
-	<!--I wanted to put an image at the top of the web page-->
-	<header>
-		<div id="wrapperHeader">
-			<div id="header">
-				<img src="images/header.png" alt="logo" />
-			</div>
-		</div>
-	</header>
+	<!--I will work on these in more detail once I know my specialization and what career path I want to take-->
 	<body>
-		<!--I will work on these in more detail once I know my specialization and what career path I want to take-->
+		<!--I wanted to put an image at the top of the web page-->
+		<header>
+			<div id="wrapperHeader">
+				<div id="header">
+					<img src="images/header.png" alt="logo" />
+				</div>
+			</div>
+		</header>
 		<h1>Purpose</h1>
 		<p>I want to learn the foundations of both front end and back end development.</p>
 		<h1>Audience</h1>
 		<p>This website will be for professional peers, developers and potential employers.</p>
 		<h1>Goal</h1>
 		<p>My goal is to obtain a quality internship or an entry level software development job.</p>
-		<!--Will update persona in more detail, I really suck at things like that-->
+		<!--Will update persona in more detail, I really suck at things like that. I'll also add an image.-->
 		<h1>Persona</h1>
 		<p>Adam is a 38 year old hiring manager at a small independent software company. He is looking for someone to hire as a junior developer. He wants to see an informative history, portfolio and contact information about this candidate. He needs an easily navigated site which works on multiple platforms. The site must show the candidates skill level or he will be immediately turned off.</p>
 	</body>
