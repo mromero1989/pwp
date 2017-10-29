78c7755cb8f02b8d874d7866ef6fcb166e4bd535
Michael Romero <m.romero1989@gmail.com>
2017-10-29 14:18:56 -0600

    Fixed some spaces

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,15 +8,17 @@
 	<body>
 		<header>
 			<h1>Milestone 2</h1>
+		</header>
 			<main>
 				<h2>Content Strategy</h2>
 				<h3>Enter Page</h3>
 				<p>Visually stimulating enter page designed to lure users into a space. It also shows what kind 				of work I am capable of doing.</p>
+
 				<h3>Gallery/About Me Section</h3>
 				<p>User is taken to a gallery where my work is displayed minimally. It shows off my portfolio and 				also lets the user learn about me.</p>
+
 				<h3>Contact Me Section</h3>
 				<p>This lets the user contact me through a contact form. I will also have links to multiple 				social media.</p>
 			</main>
-		</header>
 	</body>
 </html>
