818ab6fd35d409041d1bea7110703933df926dec
Michael Romero <m.romero1989@gmail.com>
2017-11-02 09:49:39 -0600

    Added wireframe images

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -11,14 +11,16 @@
 		</header>
 		<main>
 			<h2>Content Strategy</h2>
-			<h3>Enter Page</h3>
-			<p>Visually stimulating enter page designed to lure users into a space. It also shows what kind 				of work I am capable of doing.</p>
-
-			<h3>Gallery/About Me Section</h3>
-			<p>User is taken to a gallery where my work is displayed minimally. It shows off my portfolio and 				also lets the user learn about me.</p>
-
-			<h3>Contact Me Section</h3>
-			<p>This lets the user contact me through a contact form. I will also have links to multiple 				social media.</p>
+			<h3>About Me</h3>
+			<p>I want to have an about me section for others to learn about who I am.</p>
+			<h3>Portfolio</h3>
+			<p>As you scroll there is my portfolio. It shows off my portfolio and websites and projects I have made. I want it to be a sliding image and to link to the actual sites.</p>
+			<h3>Contact Form</h3>
+			<p>This lets the user contact me through a contact form. I will also have links to multiple 				social media. I will also have my information posted as well such as phone number and email. 	</p>
+			<h2>Desktop Wireframe</h2> <!--I need to fix somethings on here-->
+			<img src="images/pwpdesktopwireframe2.png" alt="desktop wireframe"/>
+			<h2>Mobile Wireframe</h2>
+			<img src="images/pwpmobilewireframe.png" alt="mobile wireframe"/>
 		</main>
 	</body>
 </html>
