75000fcd9fbd3cbf86052bd8eceb2963db1e49f3
Michael Romero <m.romero1989@gmail.com>
2017-11-20 07:40:05 -0700

    Added 'and' to build & work.

--- public_html/index.php
+++ public_html/index.php
@@ -13,7 +13,7 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 		<script src="https://use.fontawesome.com/21e80d15b3.js"></script>
 
-		<title>Build & Work</title>
+		<title>Build and Work</title>
 	</head>
 
 	<body>
