d6bb6b0a5ec38128ace610550a4508236e2bec42
Michael Romero <m.romero1989@gmail.com>
2017-11-19 21:43:10 -0700

    Added slider

--- public_html/index.php
+++ public_html/index.php
@@ -6,17 +6,21 @@
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"/>
 
+		<!--flickity slider-->
+		<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+
 		<link rel="stylesheet" href="documentation/css/pwpstyle.css"/>
 
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		<!--flickity js-->
+		<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 
 		<title>Build & Work</title>
 	</head>
 
 	<body>
-
 		<nav class="navbar navbar-expand-lg navbar-light style="background-color: #2B2ACE;">
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
@@ -46,20 +50,72 @@
 			</div>
 		</header>
 
-		<div class="container">
-			<div class="row">
-
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-md-12">
+						<img src="documentation/images/aboutme.jpg" rel="icon" alt="logo" class="aboutmeresize"/>
+						<div class="card">
+							<div class="card-body text-white">
+								Lorem ipsum dolor amet single-origin coffee organic lumbersexual roof party subway tile man bun fam pug cornhole squid shaman church-key you probably haven't heard of them small batch. Mlkshk semiotics next level palo santo. Venmo gentrify shaman echo park, yr tilde four dollar toast locavore fingerstache gastropub synth activated charcoal glossier bicycle rights aesthetic. Lyft franzen microdosing craft beer, cronut prism ugh artisan tofu echo park normcore cred pabst brooklyn chartreuse. Skateboard keytar direct trade, next level selvage YOLO schlitz cred etsy vinyl irony selfies copper mug poke sriracha.
+							</div>
+						</div>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
+		</section>
+
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-md-12">
+						<img src="documentation/images/portfolio.jpg" rel="icon" alt="logo" class="aboutmeresize portfolioresize"/>
+						<div class="slider-wrap">
+							<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+								<ol class="carousel-indicators">
+									<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+									<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+								</ol>
+								<div class="carousel-inner">
+									<div class="carousel-item active">
+										<img class="d-block w-100" src="http://artfucksme.com/wp-content/uploads/2014/11/darkangel.gif" alt="First slide">
+									</div>
+									<div class="carousel-item">
+										<img class="d-block w-100" src="http://artfucksme.com/wp-content/uploads/2014/11/Adam-Taylor_01_closer-look.gif" alt="Second slide">
+									</div>
+									<div class="carousel-item">
+										<img class="d-block w-100" src="http://artfucksme.com/wp-content/uploads/2014/11/Paul_Apal_kin_02.gif" alt="Third slide">
+									</div>
+								</div>
+								<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+									<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+									<span class="carousel-control-next-icon" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-md-12">
+						<img src="documentation/images/contact.jpg" alt="logo" class="aboutmeresize portfolioresize contactresize"/>
+					</div>
+				</div>
+			</div>
+		</section>
+
 	</body>
 </html>
 
-<!--<nav class="navbar navbar-light" style="background-color: #2B2ACE;">-->
-<!--	<div class="container-fluid">-->
-<!--		<div>-->
-<!--			<a class="navbar-brand text-white" href="#">About Me</a>-->
-<!--			<a class="navbar-brand text-white" href="#">Portfolio</a>-->
-<!--			<a class="navbar-brand text-white" href="#">Contact</a>-->
-<!--		</div>-->
-<!--	</div>-->
-<!--</nav>-->
+
+
